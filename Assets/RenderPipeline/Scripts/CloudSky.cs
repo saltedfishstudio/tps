253dab8554847d86cc66b592bfcbe3d87077be40
saltedfishstudio <saltedfishstudio@pm.me>
2020-03-05 18:51:13 +0900

    하늘

---
+++ Assets/RenderPipeline/Scripts/CloudSky.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+[VolumeComponentMenu("Sky/Cloud Sky")]
+[SkyUniqueID(NEW_SKY_UNIQUE_ID)]
+public class CloudSky : SkySettings
+{
+    const int NEW_SKY_UNIQUE_ID = 20382390;
+    
+    [Tooltip("Specify the cubemap HDRP uses to render the sky.")]
+    public CubemapParameter hdriSky = new CubemapParameter(null);
+
+    public override Type GetSkyRendererType()
+    {
+        return typeof(CloudSkyRenderer);
+    }
+
+    public override int GetHashCode()
+    {
+        int hash = base.GetHashCode();
+        unchecked
+        {
+            hash = hdriSky.value != null ? hash * 23 + hdriSky.GetHashCode() : hash;
+        }
+        return hash;
+    }
+}
---
+++ Assets/RenderPipeline/Scripts/CloudSkyRenderer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class CloudSkyRenderer : SkyRenderer
+{
+	public static readonly int _cubeMap = Shader.PropertyToID("_Cubemap");
+	public static readonly int _skyParam = Shader.PropertyToID("_SkyParam");
+	public static readonly int _pixelCoordToViewDirWS = Shader.PropertyToID("_PixelCoordToViewDirWS");
+
+	Material m_newSkyMaterial;
+	MaterialPropertyBlock m_propertyBlock = new MaterialPropertyBlock();
+
+	static int m_renderCubemapID = 0;
+	static int m_renderFullscreenSkyID = 1;
+	
+	
+	public override void Build()
+	{
+		m_newSkyMaterial = CoreUtils.CreateEngineMaterial(GetNewSkyShader());
+	}
+
+	public override void Cleanup()
+	{
+		CoreUtils.Destroy(m_newSkyMaterial);
+	}
+
+	protected override bool Update(BuiltinSkyParameters builtinParams)
+	{
+		return false;
+	}
+
+	public override void RenderSky(BuiltinSkyParameters builtinParams, bool renderForCubemap, bool renderSunDisk)
+	{
+		using (new ProfilingSample(builtinParams.commandBuffer, "Draw sky"))
+		{
+			var newSky = builtinParams.skySettings as CloudSky;
+
+			int passID = renderForCubemap ? m_renderCubemapID : m_renderFullscreenSkyID;
+
+			float intensity = GetSkyIntensity(newSky, builtinParams.debugSettings);
+			float phi = -Mathf.Deg2Rad * newSky.rotation.value; // -rotation to match Legacy
+			
+			m_propertyBlock.SetTexture(_cubeMap, newSky.hdriSky.value);
+			m_propertyBlock.SetVector(_skyParam, new Vector4(intensity, 0.0f, Mathf.Cos(phi), Mathf.Sin(phi)));
+			m_propertyBlock.SetMatrix(_pixelCoordToViewDirWS, builtinParams.pixelCoordToViewDirMatrix);
+
+			CoreUtils.DrawFullScreen(builtinParams.commandBuffer, m_newSkyMaterial, m_propertyBlock, passID);
+		}
+	}
+
+	Shader GetNewSkyShader()
+	{
+		// implementation
+		return Shader.Find("Hidden/HDRP/Sky/CloudSky");
+	}
+}
